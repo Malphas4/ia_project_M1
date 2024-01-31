0787466468d98147ae018e23b67c8e6a95caffaf
malphas <victorweber57@gmail.com>
2024-01-31 19:09:05 +0100

    fonction PVS contenant l'ago qui sera utilisé dans getDecision. Une option pour gagner en perf est de passer par des int au lieu des doubles, on veut stocker 0.037, on transforme en 37, 1 devient 1 000, quantisation. on verra si utile plus tard

--- src/awele/bot/competitor/PVS/PVS.java
+++ src/awele/bot/competitor/PVS/PVS.java
@@ -3,6 +3,8 @@ package awele.bot.competitor.PVS;
 import awele.bot.CompetitorBot;
 import awele.core.Board;
 
+import static java.lang.Math.max;
+
 public class PVS extends CompetitorBot {
     @Override
     public void initialize() {
@@ -23,6 +25,29 @@ public class PVS extends CompetitorBot {
     public void learn() {
 
     }
+    public int pvs(NodePVS node, int depth,int α, int β,int color) {
+        int score;
+        if( depth == 0 || node.next ==null){
+            return color * node.pvs();
+        }
+        for (NodePVS child:
+            node.childs) {
+            if (child.pred ==null )
+                score = -pvs(child, depth - 1, -β, -α, -color);
+            else {
+                score = -pvs(child, depth - 1, -α - 1, -α, -color);//(* search with a null window *)
+                if (α < score && score < β)
+                    score = -pvs(child, depth - 1, -β, -α, -color);// (* if it failed high, do a full re-search *)
+            }
+            α = max(α, score);
+            if( α >= β)
+                child=null;//break;// (* beta cut-off *)
+            //  return α;
+        }
+        return α;
+    }
+
+
     /*
     function pvs(node, depth, α, β, color) is
     if depth = 0 or node is a terminal node then
