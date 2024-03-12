adf243eb9cc070f9d6ecd935952a8a1ce8ee7103
Malphas4 <victorweber57@gmail.com>
2024-03-12 18:58:59 +0100

    MinMax avec TT jamal olek

---
+++ src/awele/bot/competitor/MinMaxModif/MinMaxModifBot.java
@@ -0,0 +1,26 @@
+package awele.bot.competitor.MinMaxModif;
+
+import awele.bot.CompetitorBot;
+import awele.core.Board;
+
+public class MinMaxModifBot extends CompetitorBot {
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void finish() {
+
+    }
+
+    @Override
+    public double[] getDecision(Board board) {
+        return new double[0];
+    }
+
+    @Override
+    public void learn() {
+
+    }
+}
